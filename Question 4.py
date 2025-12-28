68ed73cc41f77cc4a60a341dccd8b40e54a1e90e
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 18:15:58 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,25 @@
+'''
+Count Vowels and Consonants in Even Indices
+Given a string s, count the number of vowels and consonants that appear at even indices in the string (0-based indexing). Ignore non-alphabetical characters.
+
+Implement the function count_vowels_and_consonants_in_even_indices that returns the counts as described above.
+
+The string s can have both upper and lower case letters.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Examples
+
+"abcdeaeiou" -> (4, 1)
+Characters at even indices are: 'a', 'c', 'e', 'e', 'u'.
+Vowels: 'a', 'e', 'e', 'u' (4 vowels).
+Consonants: 'c' (1 consonant).
+"a11bc11de11" -> (2, 1)
+Characters at even indices are: 'a', '1', 'b', '1', 'd', '1'.
+Vowels: 'a' (1 vowel).
+Consonants: 'b', 'd' (2 consonants).
+"ABCDE" -> (2, 1)
+Characters at even indices are: 'A', 'C', 'E'.
+Vowels: 'A', 'E' (2 vowels).
+Consonants: 'C' (1 consonant).
+'''
