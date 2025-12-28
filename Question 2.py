22706567f63259e29aa7b8468931739f7c7329ee
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 18:14:22 +0530

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,15 @@
+'''
+Surround First Two and Last Two with Square Brackets
+Given a string s, you need to surround the first two and the last two characters of the string with square brackets [].
+
+Assueme the string s will have at least 4 characters.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example
+
+"hello" -> "[he]l[lo]"
+"python" -> "[py]th[on]"
+"brackets" -> "[br]acke[ts]"
+"four" -> "[fo][ur]"
+'''
