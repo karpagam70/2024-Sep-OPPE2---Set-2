d5965e2f9d424afd57635bf932b08f615debc36f
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 18:13:31 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,23 @@
+'''
+Sum of Floored to Tens
+Given two integers a and b, return the sum of the two numbers where both numbers are floored to the nearest ten.
+
+Flooring to the nearest ten means removing the unit digit and rounding the number down to the nearest multiple of 10.
+
+Assume both the numbers are positive integers.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example
+Input:
+
+35, 46
+Output:
+
+70
+Explanation:
+
+35 floored to tens is 30.
+46 floored to tens is 40.
+Sum: 30 + 40 = 70.
+'''
